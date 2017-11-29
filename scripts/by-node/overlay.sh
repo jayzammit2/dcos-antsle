15dab826f3e4c5907937bbf031ebf065cc6569f9
Jay Zammit <jayzammit2@gmail.com>
2017-11-28 23:44:18 -0500

    Modify script

--- scripts/by-node/overlay.sh
+++ scripts/by-node/overlay.sh
@@ -7,8 +7,6 @@
 #
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/7.2_release_notes/technology-preview-file_systems
 
-source ../config/env.sh
-
 tee /etc/modules-load.d/overlay.conf << '__EOF__'
 overlay
 __EOF__
