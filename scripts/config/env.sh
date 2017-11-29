ea7a8fa7826374a869fa55ac81064d1226d911fa
Jay Zammit <jayzammit2@gmail.com>
2017-11-28 20:55:08 -0500

    Add script

---
+++ scripts/config/env.sh
@@ -0,0 +1,87 @@
+# This script is used to set the environment variables that the setup scripts use to install
+# the DC/OS cluster.  By default, the cluster is set with
+#
+# 1 Master Node
+# 2 Public Agent Nodes
+# 3 Private Agent Nodes
+#
+# You are free to set this up however you like.
+# 
+# NOTE: Set the IP's to whatever you like based on your network requirements.
+#
+# For a definition of DC/OS Node types, see
+#
+# # See https://dcos.io/docs/1.9/overview/architecture/node-types/
+
+# ------ MASTER NODE IPs -------
+
+MASTER1_NODE_IP=10.1.1.11
+export MASTER1_NODE_IP
+
+# MASTER2_NODE_IP=<YOUR IP>
+# export MASTER2_NODE_IP
+
+# MASTER3_NODE_IP=<YOUR IP>
+# export MASTER3_NODE_IP
+
+# ------ PUBLIC AGENT NODE IPs -------
+
+PUBLIC_AGENT1_NODE_IP=10.1.1.12
+export PUBLIC_AGENT1_NODE_IP
+
+PUBLIC_AGENT2_NODE_IP=10.1.1.13
+export PUBLIC_AGENT2_NODE_IP
+
+# PUBLIC_AGENT3_NODE_IP=<YOUR IP>
+# export PUBLIC_AGENT3_NODE_IP
+
+# PUBLIC_AGENT4_NODE_IP=<YOUR IP>
+# export PUBLIC_AGENT4_NODE_IP
+
+# PUBLIC_AGENT5_NODE_IP=<YOUR IP>
+# export PUBLIC_AGENT5_NODE_IP
+
+# ------ PRIVATE AGENT NODE IPs -------
+
+PRIVATE_AGENT1_NODE_IP=10.1.1.14
+export PRIVATE_AGENT1_NODE_IP
+
+PRIVATE_AGENT2_NODE_IP=10.1.1.15
+export PRIVATE_AGENT2_NODE_IP
+
+PRIVATE_AGENT3_NODE_IP=10.1.1.16
+export PRIVATE_AGENT3_NODE_IP
+
+# PRIVATE_AGENT4_NODE_IP=<YOUR IP>
+# export PRIVATE_AGENT4_NODE_IP
+
+# PRIVATE_AGENT5_NODE_IP=<YOUR IP>
+# export PRIVATE_AGENT5_NODE_IP
+
+# PRIVATE_AGENT6_NODE_IP=<YOUR IP>
+# export PRIVATE_AGENT6_NODE_IP
+
+# PRIVATE_AGENT7_NODE_IP=<YOUR IP>
+# export PRIVATE_AGENT7_NODE_IP
+
+# ------ BOOT NODE IPs & PORT -------
+
+BOOT_NODE_IP=10.1.1.10
+export BOOT_NODE_IP
+
+BOOT_NODE_PORT=9000
+export BOOT_NODE_PORT
+
+# ------ MISC. SETTINGS -------
+
+# This password is used in the setup-keys.sh script to help with the automation of copying ssh keys to
+# each node in the DC/OS cluster.  If you used this it is advisable to change the password for each node VM
+# once the installation is complete.
+#
+# If you do not want to use this, that is ok as well. Modify the setup-key.sh script and simply answer each
+# prompt to log in.
+
+SSHPWD='antsle'
+export SSHPWD
+
+
