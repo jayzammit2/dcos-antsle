d4cd7446d45aedd6f6682d8bb956024d4e9a882c
Jay Zammit <jayzammit2@gmail.com>
2017-11-29 00:57:07 -0500

    Modify script

--- scripts/all-nodes/all-keys.sh
+++ scripts/all-nodes/all-keys.sh
@@ -14,3 +14,15 @@ sshpass -p ${SSHPWD} ssh-copy-id ${PRIVATE_AGENT1_NODE_IP}
 sshpass -p ${SSHPWD} ssh-copy-id ${PRIVATE_AGENT2_NODE_IP} 
 sshpass -p ${SSHPWD} ssh-copy-id ${PRIVATE_AGENT3_NODE_IP} 
 
+# Copy env.sh to each nodes for later instalation and configuration.  This 
+# may not be the best place to include this code, but it was the most convenient.
+# Some of the later instalation scripts are run remotely and they depend on the the
+# environment variables being set locally
+
+scp ../config/env.sh ${MASTER1_NODE_IP}:~/env.sh
+scp ../config/env.sh ${PUBLIC_AGENT1_NODE_IP}:~/env.sh
+scp ../config/env.sh ${PUBLIC_AGENT2_NODE_IP}:~/env.sh
+scp ../config/env.sh ${PRIVATE_AGENT1_NODE_IP}:~/env.sh
+scp ../config/env.sh ${PRIVATE_AGENT2_NODE_IP}:~/env.sh
+scp ../config/env.sh ${PRIVATE_AGENT3_NODE_IP}:~/env.sh
+
