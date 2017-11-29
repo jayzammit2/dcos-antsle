448092a86e40a34dde42dcf9355e8763bd1f797f
Jay Zammit <jayzammit2@gmail.com>
2017-11-28 21:08:15 -0500

    Added scripts

---
+++ scripts/by-node/master-node.sh
@@ -0,0 +1,8 @@
+source ../config/env.sh
+mkdir -p /opt/dcos-setup
+cd /opt/dcos-setup
+
+curl -O http://${BOOT_NODE_IP}:${BOOT_NODE_PORT}/dcos_install.sh
+chmod +x dcos_install.sh
+./dcos_install.sh master
+
