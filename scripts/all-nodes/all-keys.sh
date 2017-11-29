5544d2723c10922f811d0070c3d11d03a93107ed
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-11-29 01:01:42 -0500

    Update all-keys.sh

--- scripts/all-nodes/all-keys.sh
+++ scripts/all-nodes/all-keys.sh
@@ -17,7 +17,11 @@ sshpass -p ${SSHPWD} ssh-copy-id ${PRIVATE_AGENT3_NODE_IP}
 # Copy env.sh to each nodes for later instalation and configuration.  This 
 # may not be the best script to include this code, but it was the most convenient.
 # Some of the later instalation scripts are run remotely and they depend on the
-# environment variables being set locally
+# environment variables being set locally. The scripts include
+# 1) agent-node-private.sh
+# 2) agent-node-public.sh
+# 3) master-node.sh
+# 4) mount-overlay.sh
 
 scp ../config/env.sh ${MASTER1_NODE_IP}:~/env.sh
 scp ../config/env.sh ${PUBLIC_AGENT1_NODE_IP}:~/env.sh
