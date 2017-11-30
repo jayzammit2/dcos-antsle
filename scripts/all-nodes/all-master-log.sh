00856a78c272a3d2525c55adf0e48eb1bfa2b5a5
Jay Zammit <jayzammit2@gmail.com>
2017-11-30 13:44:58 -0500

    Modify script

--- scripts/all-nodes/all-master-log.sh
+++ scripts/all-nodes/all-master-log.sh
@@ -1,4 +1,2 @@
 source ../config/env.sh
-ssh ${MASTER1_NODE_IP} 'bash -s' < ../utils/check-master-log.sh &
-# ssh ${MASTER2_NODE_IP} 'bash -s' < ../utils/check-master-log.sh &
-# ssh ${MASTER3_NODE_IP} 'bash -s' < ../utils/check-master-log.sh &
+ssh ${MASTER1_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
