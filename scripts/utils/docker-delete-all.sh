bf831efdc11cdfed0113cbbe0375c8dec8b1ea09
Jay Zammit <jayzammit2@gmail.com>
2017-11-30 11:33:12 -0500

    Modify script

--- scripts/utils/docker-delete-all.sh
+++ scripts/utils/docker-delete-all.sh
@@ -1,6 +1,9 @@
-# Utility to stop and clean up docker processes. Not needed for the install but can be handy when troubleshooting
-# I primarily used this script when something went wrong on the boot node, and I needed to rerun the bood-node.sh script.
-# The reason being is that the script started a docker container that was then used by the Master and Agent nodes to pull
+# Utility to stop and clean up docker processes. 
+#
+# This script is not used for the install but can be handy when troubleshooting. 
+#
+# I primarily used the script when something went wrong on the boot node and needed to rerun the bood-node.sh script.
+# The reason is that the boot-node script starts a docker container that is used by the Master and Agent nodes to pull
 # the install script and self configure themselves. 
 
 docker stop $(docker ps -a -q)
