92f3c74cbb7af2bf60e2aab2f14b5a6910a1678a
Jay Zammit <jayzammit2@gmail.com>
2017-11-30 13:52:36 -0500

    Modify script

--- scripts/all-nodes/all-master-log.sh
+++ scripts/all-nodes/all-master-log.sh
@@ -2,15 +2,15 @@ source ../config/env.sh
 
 if [ $1 = '1' ]
 then
-ssh ${MASTER1_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
+  ssh ${MASTER1_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
 fi
 
-if [ $1 = '2' ]
-then
-ssh ${MASTER2_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
-fi
+# if [ $1 = '2' ]
+# then
+#   ssh ${MASTER2_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
+# fi
 
-if [ $1 = '3' ]
-then
-ssh ${MASTER3_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
-fi
+# if [ $1 = '3' ]
+# then
+#   ssh ${MASTER3_NODE_IP} 'bash -s' < ../utils/check-master-log.sh 
+# fi
