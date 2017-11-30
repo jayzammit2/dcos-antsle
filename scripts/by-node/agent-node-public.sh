8452be1eeafede52bb4e34703f8ec3c31c0eb609
Jay Zammit <jayzammit2@gmail.com>
2017-11-30 11:57:48 -0500

    Modify script

--- scripts/by-node/agent-node-public.sh
+++ scripts/by-node/agent-node-public.sh
@@ -1,3 +1,12 @@
+# This script is used to set up the Public Agent Node. Details on DC/OS Advanced Installation can be found at 
+# the following link.
+# 
+# https://dcos.io/docs/1.7/administration/installing/custom/advanced/
+
+# NOTE: The env.sh script must be present on the node in order to set the environment variables.  In the case
+# of this install, env.sh is pushed to each agent node from the a-nodes/all-keys.sh script.  
+
+
 source ./env.sh
 
 curl -O http://${BOOT_NODE_IP}:${BOOT_NODE_PORT}/dcos_install.sh
