74a03b6c9a0bdfe7a9c08f37c6207ecb3893c40c
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-12-02 11:48:23 -0500

    Update overlay.sh

--- scripts/by-node/overlay.sh
+++ scripts/by-node/overlay.sh
@@ -11,3 +11,21 @@ tee /etc/modules-load.d/overlay.conf << '__EOF__'
 overlay
 __EOF__
 
+# ------ CLUSTER PERMISSION INSTALL & SETUP ------
+# On each of your cluster nodes, use the following command to:
+# - Disable SELinux or set it to permissive mode.
+# - Add nogroup to each of your Mesos masters and agents.
+# - Reboot your cluster for the changes to take effect.
+
+sed -i s/SELINUX=enforcing/SELINUX=permissive/g /etc/selinux/config
+setenforce 0
+sestatus | grep -i mode
+groupadd nogroup
+
+# ------ LOCALE REQUIREMENTS ------
+
+tee /etc/environment << '__EOF__'
+LC_ALL="en_US.utf8"
+__EOF__
+
+# ------ END LOCALE REQUIREMENTS ------
