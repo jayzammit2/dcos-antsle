1a9f3fd74d4fadd940142d5257dd07256d2f50b3
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-12-05 12:53:30 -0500

    Update all-mount-overlay.sh

--- scripts/all-nodes/all-mount-overlay.sh
+++ scripts/all-nodes/all-mount-overlay.sh
@@ -5,3 +5,7 @@ ssh ${PUBLIC_AGENT2_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
 ssh ${PRIVATE_AGENT1_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
 ssh ${PRIVATE_AGENT2_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
 ssh ${PRIVATE_AGENT3_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
+ssh ${PRIVATE_AGENT4_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
+ssh ${PRIVATE_AGENT5_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
+ssh ${PRIVATE_AGENT6_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
+ssh ${PRIVATE_AGENT7_NODE_IP} 'bash -s' < ../by-node/mount-overlay.sh &
