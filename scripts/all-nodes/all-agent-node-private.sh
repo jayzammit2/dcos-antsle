06837e3133a9135a0bd06a5ad4540bf201b415eb
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-12-05 12:50:59 -0500

    Update all-agent-node-private.sh

--- scripts/all-nodes/all-agent-node-private.sh
+++ scripts/all-nodes/all-agent-node-private.sh
@@ -2,8 +2,8 @@ source ../config/env.sh
 ssh ${PRIVATE_AGENT1_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
 ssh ${PRIVATE_AGENT2_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
 ssh ${PRIVATE_AGENT3_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
-# ssh ${PRIVATE_AGENT4_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
-# ssh ${PRIVATE_AGENT5_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
-# ssh ${PRIVATE_AGENT6_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
-# ssh ${PRIVATE_AGENT7_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
+ssh ${PRIVATE_AGENT4_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
+ssh ${PRIVATE_AGENT5_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
+ssh ${PRIVATE_AGENT6_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
+ssh ${PRIVATE_AGENT7_NODE_IP} 'bash -s' < ../by-node/agent-node-private.sh &
 
