02534fb43b8685c8aec25d01cb7489881b86a0d6
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-12-07 14:20:29 -0500

    Update all-overlay.sh

--- scripts/all-nodes/all-overlay.sh
+++ scripts/all-nodes/all-overlay.sh
@@ -18,7 +18,7 @@ ssh ${PUBLIC_AGENT2_NODE_IP} 'bash -s' < ../by-node/overlay.sh
 ssh ${PRIVATE_AGENT1_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
 ssh ${PRIVATE_AGENT2_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
 ssh ${PRIVATE_AGENT3_NODE_IP} 'bash -s' < ../by-node/overlay.sh
-# ssh ${PRIVATE_AGENT4_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
-# ssh ${PRIVATE_AGENT5_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
+ssh ${PRIVATE_AGENT4_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
+ssh ${PRIVATE_AGENT5_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
 # ssh ${PRIVATE_AGENT6_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
 # ssh ${PRIVATE_AGENT7_NODE_IP} 'bash -s' < ../by-node/overlay.sh 
