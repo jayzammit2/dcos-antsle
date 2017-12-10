5633bc22b774272e1ae770780373981aa75ed00a
jayzammit2 <32422453+jayzammit2@users.noreply.github.com>
2017-12-09 21:31:25 -0500

    Update all-agent-node-public.sh

--- scripts/all-nodes/all-agent-node-public.sh
+++ scripts/all-nodes/all-agent-node-public.sh
@@ -1,6 +1,6 @@
 source ../config/env.sh
 ssh ${PUBLIC_AGENT1_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
-ssh ${PUBLIC_AGENT2_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
+# ssh ${PUBLIC_AGENT2_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
 # ssh ${PUBLIC_AGENT3_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
 # ssh ${PUBLIC_AGENT4_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
 # ssh ${PUBLIC_AGENT5_NODE_IP} 'bash -s' < ../by-node/agent-node-public.sh &
